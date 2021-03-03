b86dcad1e54eab577e2c548cd415885ff2ed76d4
famzil <famzil.contact@gmail.com>
2021-03-03 19:36:08 +0100

    deployment

--- server/server.js
+++ server/server.js
@@ -39,21 +39,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname + '/build/index.html'));
   });
 }
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
 
 app.use('/articles', articlesRouter);
 app.use('/contacts', contactsRouter);
