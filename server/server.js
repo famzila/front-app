78e5821624ddf233c849d1afc3482e3c7c0fa4b6
famzil <famzil.contact@gmail.com>
2021-03-04 11:46:17 +0100

    deployment

--- server/server.js
+++ server/server.js
@@ -37,9 +37,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../build')));
+  app.use(express.static(path.join(__dirname, '../build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'build/index.html'));
+    res.sendFile(path.join(__dirname + 'build','index.html'));
   });
 }
 
