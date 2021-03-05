ea79cac56f401fc803c208edfb0ab3d219458b0f
famzil <famzil.contact@gmail.com>
2021-03-05 19:04:43 +0100

    Fix client routes problem

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use('/contacts', contactsRouter);
 
 app.use(express.static(path.join(__dirname, '../build')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(port, () => {
