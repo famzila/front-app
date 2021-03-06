e26c660d204c82dfa6d9f477fd35cc3b8477425c
famzil <famzil.contact@gmail.com>
2021-03-06 12:55:29 +0100

    Fix articles url

--- src/views/ArticlesList.js
+++ src/views/ArticlesList.js
@@ -15,7 +15,7 @@ class ArticlesList extends React.Component {
   };
 
   getArticles() {
-    let base_url = '/';
+    let base_url = window.location.origin;;
     if (process.env.NODE_ENV !== 'production') {
       base_url = process.env.REACT_APP_LOCAL;
     }
